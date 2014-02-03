b784278872b1d66ce2940f4988e0479971de8bc0
0xc0170 <c0170@rocketmail.com>
2014-02-03 18:28:24 +0100

    uvision exporters for K64F, uticker - lptmr
    	- lptmr - no hal neiter driver, quick implementation using registers and internal clock
    	- exporters for K64F - using K60 1MB target, because K64F is not available in 4.7 yet

--- libraries/mbed/targets/hal/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_K64F/us_ticker.c
+++ libraries/mbed/targets/hal/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_K64F/us_ticker.c
@@ -19,6 +19,7 @@
 #include "fsl_pit_driver.h"
 #include "fsl_sim_hal.h"
 #include "fsl_clock_manager.h"
+#include "fsl_clock_configs.h"
 
 static void pit_init(void);
 static void lptmr_init(void);
@@ -68,11 +69,33 @@ static void pit_init(void) {
 static void lptmr_isr(void);
 
 static void lptmr_init(void) {
+    clock_hal_set_gate(kSimClockModuleLPTIMER, 0, true);
 
+    /* Set interrupt handler */
+    NVIC_SetVector(LPTimer_IRQn, (uint32_t)lptmr_isr);
+    NVIC_EnableIRQ(LPTimer_IRQn);
+
+    /* TODO: check clock manager, due to nonstandard 50 MHz */
+    //No suitable external oscillator clock -> Use fast internal oscillator (4MHz / divider)
+    MCG->C1 |= MCG_C1_IRCLKEN_MASK;
+    MCG->C2 |= MCG_C2_IRCS_MASK;
+    LPTMR0->PSR =  LPTMR_PSR_PCS(0);
+    switch (MCG->SC & MCG_SC_FCRDIV_MASK) {
+        case MCG_SC_FCRDIV(0):                  //4MHz
+            LPTMR0->PSR |= LPTMR_PSR_PRESCALE(1);
+            break;
+        case MCG_SC_FCRDIV(1):                  //2MHz
+            LPTMR0->PSR |= LPTMR_PSR_PRESCALE(0);
+            break;
+        default:                                //1MHz or anything else, in which case we put it on 1MHz
+            MCG->SC &= ~MCG_SC_FCRDIV_MASK;
+            MCG->SC |= MCG_SC_FCRDIV(2);
+            LPTMR0->PSR |= LPTMR_PSR_PBYP_MASK;
+    }
 }
 
 void us_ticker_disable_interrupt(void) {
-
+    BW_LPTMR_CSR_TIE(0);
 }
 
 void us_ticker_clear_interrupt(void) {
@@ -83,14 +106,48 @@ static uint32_t us_ticker_int_counter = 0;
 static uint16_t us_ticker_int_remainder = 0;
 
 static void lptmr_set(unsigned short count) {
-    /* Reset */
-
+    HW_LPTMR_CSR_WR(0);
+    BW_LPTMR_CMR_COMPARE(count);
+    BW_LPTMR_CSR_TIE(1);
+    BW_LPTMR_CSR_TEN(1);
 }
 
 static void lptmr_isr(void) {
+    // write 1 to TCF to clear the LPT timer compare flag
+    BW_LPTMR_CSR_TCF(1);
 
+    if (us_ticker_int_counter > 0) {
+        lptmr_set(0xFFFF);
+        us_ticker_int_counter--;
+
+    } else {
+        if (us_ticker_int_remainder > 0) {
+            lptmr_set(us_ticker_int_remainder);
+            us_ticker_int_remainder = 0;
+
+        } else {
+            // This function is going to disable the interrupts if there are
+            // no other events in the queue
+            us_ticker_irq_handler();
+        }
+    }
 }
 
 void us_ticker_set_interrupt(unsigned int timestamp) {
+    int delta = (int)(timestamp - us_ticker_read());
+    if (delta <= 0) {
+        // This event was in the past:
+        us_ticker_irq_handler();
+        return;
+    }
 
+    us_ticker_int_counter   = (uint32_t)(delta >> 16);
+    us_ticker_int_remainder = (uint16_t)(0xFFFF & delta);
+    if (us_ticker_int_counter > 0) {
+        lptmr_set(0xFFFF);
+        us_ticker_int_counter--;
+    } else {
+        lptmr_set(us_ticker_int_remainder);
+        us_ticker_int_remainder = 0;
+    }
 }
