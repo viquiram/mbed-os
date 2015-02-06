9b0cb095f619c6c56745e219a49855920b88b0fe
PeterConheeney <peter.conheeney@arm.com>
2015-02-06 11:44:52 +0000

    removed target specific code from mbed hal, added mps2 library files

--- libraries/MPS2_peripherals/mps2_ethernet_api.h
+++ libraries/MPS2_peripherals/mps2_ethernet_api.h
@@ -13,8 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef MBED_ETHERNET_API_H
-#define MBED_ETHERNET_API_H
+#ifndef MPS2_ETHERNET_API_H
+#define MPS2_ETHERNET_API_H
 
 #include "device.h"
 
@@ -26,32 +26,20 @@ extern "C" {
 
 // Connection constants
 
-int ethernet_init(void);
-void ethernet_free(void);
 
-// write size bytes from data to ethernet buffer
-// return num bytes written
-// or -1 if size is too big
-int ethernet_write(const char *data, int size);
 
 // send ethernet write buffer, returning the packet size sent
-int ethernet_send(void);
+int ethernet_transmission(unsigned char * pkt, unsigned int length);
 
 // recieve from ethernet buffer, returning packet size, or 0 if no packet
-int ethernet_receive(void);
-
-// read size bytes in to data, return actual num bytes read (0..size)
-// if data == NULL, throw the bytes away
-int ethernet_read(char *data, int size);
+int ethernet_reception(unsigned int *recvbuf, unsigned int *index);
 
 // get the ethernet address
-void ethernet_address(char *mac);
+int ethernet_mac_address(char *mac);
 
-// see if the link is up
-int ethernet_link(void);
+unsigned int ethernet_check_ready(void);
 
-// force link settings
-void ethernet_set_link(int speed, int duplex);
+unsigned int ethernet_intf(void);
 
 #ifdef __cplusplus
 }
