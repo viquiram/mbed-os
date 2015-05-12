da5d611165771fb1611df3ac21343317a3499ba9
Mihail Stoyanov <wm@smode.org>
2015-05-12 16:50:54 +0300

    Fixed targets

--- workspace_tools/targets.py
+++ workspace_tools/targets.py
@@ -1168,76 +1168,6 @@ class RZ_A1H(Target):
         self.default_toolchain = "ARM"
         
 
-
-### Silicon Labs ###
-class EFM32_G8XX_STK(Target):
-    def __init__(self):
-        Target.__init__(self)
-        self.core = "Cortex-M3"
-        self.extra_labels = ['Silicon_Labs', 'EFM32']
-        self.macros = ['EFM32G890F128']
-        self.supported_toolchains = ["GCC_ARM", "ARM", "uARM"]
-        self.default_toolchain = "ARM"
-
-
-class EFM32GG_STK3700(Target):
-    def __init__(self):
-        Target.__init__(self)
-        self.core = "Cortex-M3"
-        self.extra_labels = ['Silicon_Labs', 'EFM32']
-        self.macros = ['EFM32GG990F1024']
-        self.supported_toolchains = ["GCC_ARM", "ARM", "uARM"]
-        self.default_toolchain = "ARM"
-
-
-class EFM32LG_STK3600(Target):
-    def __init__(self):
-        Target.__init__(self)
-        self.core = "Cortex-M3"
-        self.extra_labels = ['Silicon_Labs', 'EFM32']
-        self.macros = ['EFM32LG990F256']
-        self.supported_toolchains = ["GCC_ARM", "ARM", "uARM"]
-        self.default_toolchain = "ARM"
-
-
-class EFM32TG_STK3300(Target):
-    def __init__(self):
-        Target.__init__(self)
-        self.core = "Cortex-M3"
-        self.extra_labels = ['Silicon_Labs', 'EFM32']
-        self.macros = ['EFM32TG840F32']
-        self.supported_toolchains = ["GCC_ARM", "uARM"]
-        self.default_toolchain = "ARM"
-
-
-class EFM32WG_STK3800(Target):
-    def __init__(self):
-        Target.__init__(self)
-        self.core = "Cortex-M4F"
-        self.extra_labels = ['Silicon_Labs', 'EFM32']
-        self.macros = ['EFM32WG990F256']
-        self.supported_toolchains = ["GCC_ARM", "ARM", "uARM"]
-        self.default_toolchain = "ARM"
-
-
-class EFM32ZG_STK3200(Target):
-    def __init__(self):
-        Target.__init__(self)
-        self.core = "Cortex-M0+"
-        self.extra_labels = ['Silicon_Labs', 'EFM32']
-        self.macros = ['EFM32ZG222F32']
-        self.supported_toolchains = ["GCC_ARM", "uARM"]
-        self.default_toolchain = "ARM"
-
-class EFM32HG_STK3400(Target):
-    def __init__(self):
-        Target.__init__(self)
-        self.core = "Cortex-M0+"
-        self.extra_labels = ['Silicon_Labs', 'EFM32']
-        self.macros = ['EFM32HG322F64']
-        self.supported_toolchains = ["GCC_ARM", "uARM"]
-        self.default_toolchain = "ARM"
-
 ### Maxim Integrated ###
 
 class MAXWSNENV(Target):
@@ -1258,7 +1188,9 @@ class MAX32600MBED(Target):
         self.supported_toolchains = ["GCC_ARM", "IAR", "ARM"]
         self.default_toolchain = "ARM"
 
+        
 ### Silicon Labs ###
+
 class EFM32GG_STK3700(Target):
     def __init__(self):
         Target.__init__(self)
@@ -1423,15 +1355,6 @@ TARGETS = [
 
     ### Renesas ###
     RZ_A1H(),
-    
-    ### Silicon_Labs ###
-    EFM32_G8XX_STK(),
-    EFM32GG_STK3700(),
-    EFM32LG_STK3600(),
-    EFM32TG_STK3300(),
-    EFM32WG_STK3800(),
-    EFM32ZG_STK3200(),
-    EFM32HG_STK3400(),
 
     ### Maxim Integrated ###
     MAXWSNENV(),
